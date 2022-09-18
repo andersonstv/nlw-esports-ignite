31194cd39feb50b970a39fbf9ab9dac6e70f8b38
Anderson Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2022-09-17 23:50:50 -0300

    notifications setup

--- esports/mobile/App.tsx
+++ esports/mobile/App.tsx
@@ -5,6 +5,15 @@ import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
+import {Subscription} from 'expo-modules-core'
+import * as Notifications from 'expo-notifications';
+
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+import { useEffect, useRef } from 'react';
+
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular, 
@@ -12,6 +21,30 @@ export default function App() {
     Inter_700Bold, 
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  })
+
+  useEffect(() =>{
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification =>{
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response =>{
+      console.log(response)
+    });
+
+    return () =>{
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  },[])
 
   return (
     <Background>
